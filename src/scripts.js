3aec8fe3baf2d91d6832f41092ed3d0c279327d4
Jenn Yacoubian <jyacoubian@gmail.com>
2022-10-19 11:53:18 -0600

    Organize scripts file

--- src/scripts.js
+++ src/scripts.js
@@ -3,6 +3,15 @@ import apiCalls from './apiCalls';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
+// ~~~~~~~~~~~~~~ Query Selectors ~~~~~~~~~~~~~~~~~~~~
+
+
+
+// ~~~~~~~~~~~~~~ Event Listeners ~~~~~~~~~~~~~~~~~~~~
+
+
+
+// ~~~~~~~~~~~~~~ Functions/Event Handlers ~~~~~~~~~~~~~~~~~~~~
 
 
 console.log('Hello world');
