f8bf494366b68701d5e50432424c1f49ecb24d75
Lauren Frazier <lauren.e.frazier@gmail.com>
2022-10-19 12:03:45 -0600

    Write implementation code to add and remove recipes

--- src/classes/User.js
+++ src/classes/User.js
@@ -5,6 +5,13 @@ class User {
         this.pantry = data.pantry;
         this.recipesToCook = [];
     }
+    addRecipesToCook(recipe) {
+        return this.recipesToCook.push(recipe);
+    }
+    removeRecipesToCook(recipe) {
+        this.recipesToCook.splice(recipe, 1)
+        return this.recipesToCook
+    }
 }
 
 export default User;
