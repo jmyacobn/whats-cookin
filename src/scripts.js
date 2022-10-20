4b3e96e9942266fb97751af010fe1064250d263f
Keyanna Kiko Pausch <knpausch@gmail.com>
2022-10-19 17:53:20 -0700

    WIP connecting the files to each other

--- src/scripts.js
+++ src/scripts.js
@@ -1,17 +1,22 @@
-import './styles.css';
 import apiCalls from './apiCalls';
+import './styles.css';
+import RecipeRepository from './classes/RecipeRepository';
+import Recipe from "./classes/Recipe";
+import { sampleRecipeData } from './data/sample-data';
+
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
+// ~~~~~~~~~~~~~~ Global Variables ~~~~~~~~~~~~~~~~~~~~
+let recipeRepositoryTest = new RecipeRepository(sampleRecipeData);
+
 // ~~~~~~~~~~~~~~ Query Selectors ~~~~~~~~~~~~~~~~~~~~
-
-
+// const test = document.querySelector("#recipeRepository")
 
 // ~~~~~~~~~~~~~~ Event Listeners ~~~~~~~~~~~~~~~~~~~~
+window.addEventListener('load', doSomething)
 
-
-
-// ~~~~~~~~~~~~~~ Functions/Event Handlers ~~~~~~~~~~~~~~~~~~~~
-
-
-console.log('Hello world');
+// ~~~~~~~~~~~~~~ Functions ~~~~~~~~~~~~~~~~~~~~
+function doSomething() {
+    console.log("hello cat!")
+}
