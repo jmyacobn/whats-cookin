3285856f1b44631a225329c4b8958ca88503d30c
Lauren Frazier <lauren.e.frazier@gmail.com>
2022-10-20 14:34:56 -0600

    LiveShare test

--- src/scripts.js
+++ src/scripts.js
@@ -5,7 +5,7 @@ import Recipe from "./classes/Recipe";
 import { sampleRecipeData } from './data/sample-data';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
-
+var test
 // As a user, I should be able to view a list of all recipes.
 // As a user, I should be able to click on a recipe to view more information including directions, ingredients needed, and total cost.
 // As a user, I should be able to filter recipes by a tag. (Extension option: by multiple tags)
