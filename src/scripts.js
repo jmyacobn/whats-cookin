a8f137189051a1f8f5aeeea7423d991a145aacbe
Keyanna Kiko Pausch <knpausch@gmail.com>
2022-10-19 18:50:09 -0700

    Improve innerHTML on displayAllRecipes method without alt tag

--- src/scripts.js
+++ src/scripts.js
@@ -3,28 +3,28 @@ import './styles.css';
 import RecipeRepository from './classes/RecipeRepository';
 import Recipe from "./classes/Recipe";
 import { sampleRecipeData } from './data/sample-data';
-
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
-
-// ~~~~~~~~~~~~~~ Global Variables ~~~~~~~~~~~~~~~~~~~~
-// let recipeRepositoryTest = new RecipeRepository(sampleRecipeData);
-let recipeRepository;
-
-// ~~~~~~~~~~~~~~ Query Selectors ~~~~~~~~~~~~~~~~~~~~
-const allRecipes = document.querySelector("#recipeRepository")
-
-// ~~~~~~~~~~~~~~ Event Listeners ~~~~~~~~~~~~~~~~~~~~
-window.addEventListener('load', displayAllRecipes)
-
-// ~~~~~~~~~~~~~~ Functions ~~~~~~~~~~~~~~~~~~~~
-function displayAllRecipes() {
-    recipeRepository = new RecipeRepository(sampleRecipeData)
-    let images = recipeRepository.recipes.map(recipe => recipe.image)
-    .forEach((image) => {allRecipes.innerHTML += `<img src="${image}"alt="A gear with a T in the center">`})
-}
 
 // As a user, I should be able to view a list of all recipes.
 // As a user, I should be able to click on a recipe to view more information including directions, ingredients needed, and total cost.
 // As a user, I should be able to filter recipes by a tag. (Extension option: by multiple tags)
 // As a user, I should be able to search recipes by their name. (Extension option: by name or ingredients)
+
+// ~~~~~~~~~~~~~~ Global Variables ~~~~~~~~~~~~~~~~~~~~
+let recipeRepository;
+
+// ~~~~~~~~~~~~~~ Query Selectors ~~~~~~~~~~~~~~~~~~~~
+const allRecipes = document.querySelector("#recipeRepository");
+
+// ~~~~~~~~~~~~~~ Event Listeners ~~~~~~~~~~~~~~~~~~~~
+window.addEventListener('load', displayAllRecipes);
+
+// ~~~~~~~~~~~~~~ Functions ~~~~~~~~~~~~~~~~~~~~
+function displayAllRecipes() {
+    recipeRepository = new RecipeRepository(sampleRecipeData);
+    let images = recipeRepository.recipes.map(recipe => recipe.image)
+    .forEach((imageUrl) => {
+        allRecipes.innerHTML += `<img src="${imageUrl}">`
+    })
+}
