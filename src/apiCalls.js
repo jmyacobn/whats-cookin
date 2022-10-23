0e981688b4b0410a9ff77463b360b20aff3e9661
Eleanor Ruth <ellie.ruth@me.com>
2022-10-22 21:30:25 -0600

    Fix fetch functions

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,9 +1,22 @@
 // Your fetch requests will live here!
-function getData(url) {
-    return fetch(url)
-    .then(response => response.json())
-    .catch(error => console.log(error))
-}
-export default getData
 
-console.log('I will be a fetch request!')
+let getRecipeData = fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes")
+    .then(response => response.json())
+    .then(data => {
+        return data})
+    .catch(err => console.log(err))
+
+let getIngredientsData = fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients")
+    .then(response => response.json())
+    .then(data => {return data})
+    .catch(err => console.log(err))
+
+let getUserData = fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users")
+    .then(response => response.json())
+    .then(data => {return data})
+    .catch(err => console.log(err))
+
+
+
+export {getRecipeData, getIngredientsData, getUserData}
+
