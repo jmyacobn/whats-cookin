06f0dd801092ce18c2b1c29b60554ae1551e3767
Eleanor Ruth <ellie.ruth@me.com>
2022-10-22 21:20:11 -0600

    Refactor fetch calls to a function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,9 @@
 // Your fetch requests will live here!
-
+function getData(url) {
+    return fetch(url)
+    .then(response => response.json())
+    .catch(error => console.log(error))
+}
+export default getData
 
 console.log('I will be a fetch request!')
