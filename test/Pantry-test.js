2cbb7e60969fe1819674e6b71c9a679d79a7a9dc
Jenn Yacoubian <jyacoubian@gmail.com>
2022-11-01 15:44:18 -0600

    Setup pantry tests

---
+++ test/Pantry-test.js
@@ -0,0 +1,26 @@
+import { expect } from 'chai'
+import Pantry from '../src/classes/Pantry'
+import User from '../src/classes/User'
+import Recipe from '../src/classes/Recipe'
+import { sampleIngredientsData, sampleUsersData, sampleRecipeData } from '../src/data/sample-data'
+
+describe('Pantry', () => {
+    sampleIngredientsData
+    sampleUsersData
+    sampleRecipeData
+    let recipe1, recipe2, user1, user2
+
+beforeEach(() => {
+    user1 = new User(sampleUsersData[0])
+    user2 = new User(sampleUsersData[1])
+    recipe1 = new Recipe(sampleRecipeData[0])
+    recipe2 = new Recipe(sampleRecipeData[1])
+    sampleIngredientsData
+    sampleUsersData
+    sampleRecipeData
+  })
+
+it('Should be a function', () => {
+    expect(Pantry).to.be.a('function')
+  })
+})
