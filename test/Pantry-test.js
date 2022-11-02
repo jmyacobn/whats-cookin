14c2f19ff540a53dbd2387fc107ab46996ef4668
Eleanor Ruth <ellie.ruth@me.com>
2022-11-02 16:51:03 -0600

    Fix pantry test

--- test/Pantry-test.js
+++ test/Pantry-test.js
@@ -37,7 +37,7 @@ it('should hold property of ingredientsNeeded which starts out empty', () => {
     expect(user1.pantry.determineIngredientsNeeded(recipe2)).to.deep.equal([{missingIngredient: 1009016, quantityNeeded: 1.5 }, {missingIngredient: 20027, quantityNeeded: 1 }, {missingIngredient: 1002046, quantityNeeded: 1 }])
     expect(user2.pantry.determineIngredientsNeeded(recipe2)).to.deep.equal([])
   })
-  it.only('Should checkPantryForIngredients', () => {
+  it('Should checkPantryForIngredients', () => {
     user1.pantry.checkPantryForIngredients(recipe1)
     expect(user1.pantry.userCanCook).to.equal(true)
     user1.pantry.checkPantryForIngredients(recipe2)
